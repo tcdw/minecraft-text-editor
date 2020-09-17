db8d0a7463def36b56352b55e55b8f2a77b07069
tcdw <tcdw2011@gmail.com>
2020-09-17 18:31:47 +0800

    parser

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,40 @@
-const a: number = 42;
-document.write(`<h1>${a}</h1>`);
+const content = document.getElementById('content') as HTMLDivElement;
+const parseBtn = document.getElementById('parse') as HTMLButtonElement;
+
+interface StringItem {
+    text: string;
+    color: string;
+    bold: boolean;
+    italic: boolean;
+    underline: boolean;
+    strikethrough: boolean;
+}
+
+function parse(el: HTMLElement) {
+    let item: StringItem[] = [];
+    el.childNodes.forEach((e) => {
+        if (e.nodeType === Node.TEXT_NODE) {
+            const styles = window.getComputedStyle(el);
+            const color = styles.color;
+            const bold = Number(styles.fontWeight) >= 700;
+            const italic = (styles.fontStyle === 'italic') || (styles.fontStyle === 'oblique');
+            const underline = styles.textDecorationLine === 'underline';
+            const strikethrough = styles.textDecorationLine === 'line-through';
+            item.push({
+                text: e.nodeValue as string,
+                color,
+                bold,
+                italic,
+                underline,
+                strikethrough,
+            });
+        } else if (e.nodeType === Node.ELEMENT_NODE) {
+            item = item.concat(parse(e as HTMLElement));
+        }
+    });
+    return item;
+}
+
+parseBtn.addEventListener('click', () => {
+    console.log(parse(content));
+});
