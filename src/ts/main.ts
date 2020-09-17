8f65d6e0d30cea5e39dd5cd4a28870dda1b0890b
tcdw <tcdw2011@gmail.com>
2020-09-17 22:38:39 +0800

    strip

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,6 @@
 const content = document.getElementById('content') as HTMLDivElement;
 const parseBtn = document.getElementById('parse') as HTMLButtonElement;
+const stripBtn = document.getElementById('strip') as HTMLButtonElement;
 
 interface StringItem {
     text: string;
@@ -18,8 +19,8 @@ function parse(el: HTMLElement) {
             const color = styles.color;
             const bold = Number(styles.fontWeight) >= 700;
             const italic = (styles.fontStyle === 'italic') || (styles.fontStyle === 'oblique');
-            const underline = styles.textDecorationLine === 'underline';
-            const strikethrough = styles.textDecorationLine === 'line-through';
+            const underline = styles.textDecorationLine.includes('underline');
+            const strikethrough = styles.textDecorationLine.includes('line-through');
             item.push({
                 text: e.nodeValue as string,
                 color,
@@ -35,6 +36,62 @@ function parse(el: HTMLElement) {
     return item;
 }
 
+function strip(el: HTMLElement) {
+    const textTree = parse(el);
+    optimizeTree(textTree);
+    while (el.childNodes.length > 0) {
+        el.removeChild(el.lastChild as ChildNode);
+    }
+    textTree.forEach((e) => {
+        const span = document.createElement('span') as HTMLSpanElement;
+        span.textContent = e.text;
+        span.style.color = e.color;
+        if (e.bold) {
+            span.style.fontWeight = '700';
+        }
+        const line: string[] = [];
+        if (e.underline) {
+            line.push('underline');
+        }
+        if (e.strikethrough) {
+            line.push('line-through');
+        }
+        span.style.textDecorationLine = line.join(' ');
+        if (e.italic) {
+            span.style.fontStyle = 'italic';
+        }
+        el.appendChild(span);
+    });
+}
+
+function optimizeTree(item: StringItem[]) {
+    let i = 0;
+    while (i < item.length) {
+        if (i <= 0) {
+            i = 1;
+            continue;
+        }
+        if (item[i].color === item[i - 1].color &&
+            item[i].bold === item[i - 1].bold &&
+            item[i].italic === item[i - 1].italic &&
+            item[i].strikethrough === item[i - 1].strikethrough &&
+            item[i].underline === item[i - 1].underline) {
+            item[i - 1].text += item[i].text;
+            item = item.splice(i, 1);
+            continue;
+        }
+        i += 1;
+    }
+}
+
 parseBtn.addEventListener('click', () => {
     console.log(parse(content));
+});
+
+stripBtn.addEventListener('click', () => {
+    strip(content);
+});
+
+content.addEventListener('blur', () => {
+    strip(content);
 });
