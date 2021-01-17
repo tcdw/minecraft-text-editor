59462ed1b4bc4b1015d84c914aadad98ca26cc66
tcdw <tcdw2011@gmail.com>
2021-01-17 10:25:43 +0000

    minor fix

--- src/App.vue
+++ src/App.vue
@@ -74,6 +74,15 @@ input, button, textarea {
     position: fixed;
     width: 100vw;
     height: 100vh;
-    background-color: rgba(0, 0, 0, 0.4);
+    background-color: rgba(0, 0, 0, 0);
+    left: 0;
+    top: 0;
+    z-index: 9999;
+    transition: background-color 0.3s;
+    pointer-events: none;
+    &.active {
+        background-color: rgba(0, 0, 0, 0.4);
+        pointer-events: all;
+    }
 }
 </style>
