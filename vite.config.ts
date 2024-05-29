dc938da1efdd074734c09db477285c1c2eec9799
tcdw <tcdw2011@gmail.com>
2024-05-29 18:01:17 +0800

    Remove old code and begin rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite';
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '',
+  plugins: [react()],
 });
